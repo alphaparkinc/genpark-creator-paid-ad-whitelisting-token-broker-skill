42897dcb23394c67a7e1d9ce58454f03238f9703
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-09-01 12:43:08 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,10 @@
+class CreatorPaidAdWhitelistingTokenBrokerClient:
+    def broker_ad_whitelisting_token(self, creator_handle='@fitness_guru', platform_ad_network='TIKTOK_SPARK_ADS_AUTH', access_duration_days=60):
+        return {
+            'token_broker_id': 'tok_brk_7721',
+            'platform_network': platform_ad_network,
+            'oauth_token_encrypted_hash': 'enc_sec_8849af1209cc',
+            'identity_handshake_verified': True,
+            'active_whitelisted_ad_codes_count': 3,
+            'ad_manager_integration_url': 'https://ads.genpark.ai/whitelist/7721'
+        }
